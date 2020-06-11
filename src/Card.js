de02aecb6955ed0075b50d35bc0d05b75a6035bf
Kalyan-2707 <52063829+Kalyan-2707@users.noreply.github.com>
2020-06-11 12:51:23 +0530

    third commit

--- src/Card.js
+++ src/Card.js
@@ -6,24 +6,28 @@ function Card(props)
     if (props.weather.main)
     return (
            
-        
+        <div className="container">
         <div className="card">
-            <div className="card-item ">
+            <div className="card-content center-align">
+                <div className="card-title card-item">
                 <span className="country">
                     {props.weather.name}
                 </span>
-                <sup>
+                <sup className="btn-floating btn-small  orange">
                     {props.weather.sys.country}
                 </sup>
-                </div>
+                    </div>
+                
             <div className="card-item">
                 {props.weather.main.temp}
-                <sup>
+                <sup className="btn-floating btn-small blue disabled">
                     &deg;C
                 </sup>
                 </div>
             <div className="card-item"><img src={"http://openweathermap.org/img/w/"+ props.weather.weather[0].icon + ".png"} alt="icon"/></div>
             <div className="desc">{props.weather.weather[0].description}</div>
+        </div>
+        </div>
         </div>
 
     );
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,9 +4,15 @@ import React from "react"
 function SearchBar(props)
 {
     return(
-        <div >
-            <input className="form-control" onKeyPress={props.Fetch} type="text" value={props.query} onChange={(e) => props.setQuery(e.target.value)} name="search" id="searchbar"/>
-           
+        <div className="container valign-wrapper center-align">
+            <input className="input-field"
+             onKeyPress={props.Fetch}
+              type="text" value={props.query} 
+              onChange={(e) => props.setQuery(e.target.value)} 
+              name="search" 
+              id="searchbar"
+              
+              />                      
         </div>
     );
 }
